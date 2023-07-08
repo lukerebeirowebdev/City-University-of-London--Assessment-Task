b8b5f55b0d4a8562234a5469a2fe4ed5ac365f6a
Luke Rebeiro <68423198+lukerebeirowebdev@users.noreply.github.com>
2023-07-08 21:50:06 +0100

    added eventlistener on click which enlarges the image

--- pictures.js
+++ pictures.js
@@ -4,11 +4,19 @@ fetch('https://jsonplaceholder.typicode.com/photos')
         data.forEach(photo => {
             const img = document.createElement('img');
             img.src = photo.thumbnailUrl;
-
+            img.alt = photo.title;
+            img.title = photo.title;
+            img.classList.add('photo');
+            img.addEventListener('click', () => {
+                window.location.href = photo.url;
+            });
             const container = document.getElementById('photos-container');
             container.appendChild(img);
+            console.log(img);
+            console.log(container);
         });
     })
     .catch(error => {
         console.log('Error:', error);
+        console.log(error);
     });
